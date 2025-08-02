d9bed11255df4d4b9160bf20f9190be4ed5b3bd9
lch <chlee1925@gmail.com>
2025-08-02 22:03:12 +0900

    [UI/ 상단 프로필영역/ level util / user model]
    - 프로필 영역 레벨사진/닉네임/레벨/한줄소개
    - 레벨별 이미지/ 레벨이름 (임시)
    - 유저 모델 추가

--- app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
@@ -1,15 +1,34 @@
 package com.mukmuk.todori.ui.screen.mypage
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.mukmuk.todori.data.model.User
+import com.mukmuk.todori.ui.mypage.component.ProfileSection
+import com.mukmuk.todori.ui.theme.Dimens
+import com.mukmuk.todori.ui.theme.TodoriTheme
 
 @Composable
 fun MyPageScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("마이페이지 화면")
+    // 테스트 유저
+    val currentUser = User(
+        uid = "123",
+        nickname = "asd",
+        intro = "asdasd",
+        level = 3,
+        rewardPoint = 1200
+    )
+
+    TodoriTheme {
+        Scaffold { padding ->
+            Column(
+                modifier = Modifier
+                    .padding(Dimens.Medium)
+            ) {
+                ProfileSection(user = currentUser)
+            }
+        }
     }
 }
