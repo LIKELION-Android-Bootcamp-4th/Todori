41ea4e5786b4539b37f386c43317a051273be106
lch <chlee1925@gmail.com>
2025-08-02 22:42:49 +0900

    [UI/ 카드 메뉴 (레벨/목표/프로필)] - 나의 레벨/ 완료한 목표/ 프로필 관리 카드 메뉴

--- app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
@@ -1,12 +1,16 @@
 package com.mukmuk.todori.ui.screen.mypage
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.mukmuk.todori.data.model.User
 import com.mukmuk.todori.ui.mypage.component.ProfileSection
+import com.mukmuk.todori.ui.screen.mypage.component.MyPageMenuSection
 import com.mukmuk.todori.ui.theme.Dimens
 import com.mukmuk.todori.ui.theme.TodoriTheme
 
@@ -28,6 +32,14 @@ fun MyPageScreen() {
                     .padding(Dimens.Medium)
             ) {
                 ProfileSection(user = currentUser)
+
+                Spacer(modifier = Modifier.height(Dimens.XXLarge))
+
+                MyPageMenuSection(
+                    onLevelClick = {},
+                    onGoalClick = {},
+                    onProfileEditClick = {}
+                )
             }
         }
     }
