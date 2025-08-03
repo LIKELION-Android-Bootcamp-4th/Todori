e20034a6889297caf41387095d0c3c463b19e837
mninm <kyhee2001@naver.com>
2025-08-03 19:25:43 +0900

    [UI/MONTH 카드 추가]

--- app/src/main/java/com/mukmuk/todori/ui/screen/stats/tab/MonthTab.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/stats/tab/MonthTab.kt
@@ -1,15 +1,26 @@
 package com.mukmuk.todori.ui.screen.stats.tab
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.mukmuk.todori.ui.screen.stats.card.MonthCard
+import com.mukmuk.todori.ui.screen.stats.card.MonthProgress
+import com.mukmuk.todori.ui.screen.stats.card.SelectedMonth
+import com.mukmuk.todori.ui.theme.Dimens
 
 @Composable
 fun MonthTab() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("월간 화면")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SelectedMonth()
+        MonthCard()
+        Spacer(modifier = Modifier.height(Dimens.Large))
+        MonthProgress()
     }
 }
