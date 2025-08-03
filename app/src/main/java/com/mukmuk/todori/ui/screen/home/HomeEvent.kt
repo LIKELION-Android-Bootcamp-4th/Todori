04316d51872661ab9d3582d1bf2db913c438c60a
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-08-03 16:14:37 +0900

    fix : Event, State 구조 변경
    Timer Status 분리

--- app/src/main/java/com/mukmuk/todori/ui/screen/home/HomeEvent.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/home/HomeEvent.kt
@@ -2,7 +2,8 @@ package com.mukmuk.todori.ui.screen.home
 
 sealed class TimerEvent {
     object Start : TimerEvent()
-    object Resume : TimerEvent()
     object Stop : TimerEvent()
     object Reset : TimerEvent()
+    object Resume : TimerEvent()
+    object Record : TimerEvent()
 }
--- app/src/main/java/com/mukmuk/todori/ui/screen/home/HomeState.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/home/HomeState.kt
@@ -4,11 +4,19 @@ enum class PomodoroTimerMode {
     FOCUSED, SHORT_RESTED, LONG_RESTED
 }
 
+enum class TimerStatus {
+    IDLE,      // 초기 상태, 정지 상태
+    RUNNING,   // 타이머 동작 중
+    RECORDING  // 기록 버튼 눌러 멈춘 상태
+}
+
 data class TimerState(
     val timeLeftInMillis: Long = 1 * 60 * 1000L,
     val totalStudyTimeMills: Long = 0 * 60 * 1000L,
-    val isRunning: Boolean = false,
+    val totalRecordTimeMills: Long = 0 * 60 * 1000L,
+    val status: TimerStatus = TimerStatus.IDLE,
     val pomodoroMode: PomodoroTimerMode = PomodoroTimerMode.FOCUSED,
     val completedFocusCycles: Int = 0,
-    val isPomodoroEnabled: Boolean = true
+    val isPomodoroEnabled: Boolean = true,
+    val selectedTodoIndex: Int? = null
 )
