97412d75f6c87b5e56da75da00944949cc6af650
mninm <kyhee2001@naver.com>
2025-08-03 18:48:01 +0900

    [UI/WEEK 탭 수정]

---
+++ app/src/main/java/com/mukmuk/todori/ui/screen/stats/card/SelectedWeek.kt
@@ -0,0 +1,60 @@
+package com.mukmuk.todori.ui.screen.stats.card
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.mukmuk.todori.R
+import com.mukmuk.todori.ui.theme.AppTextStyle
+import com.mukmuk.todori.ui.theme.Dimens
+
+@Composable
+fun SelectedWeek() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(Dimens.Medium)
+    ) {
+        //왼쪽 화살표
+        Row(
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.icon_arrow_left),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+
+        //주 표시
+        Row(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text("2025년 8월 1주차", style = AppTextStyle.TitleSmall)
+        }
+
+        //오른쪽 화살표
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.icon_arrow_right),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
+}
