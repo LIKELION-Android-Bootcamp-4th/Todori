8aa3dc7d304f09655e7ed91aceea812d8c08d289
lch <chlee1925@gmail.com>
2025-08-05 09:41:45 +0900

    [Feat/MYP001] 프로필 조회 - 더미데이터 > 파이어베이스 테스트 데이터로 교체중

--- app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
@@ -6,8 +6,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mukmuk.todori.data.remote.user.User
 import com.mukmuk.todori.ui.mypage.component.ProfileSection
 import com.mukmuk.todori.ui.screen.mypage.component.MyPageAccountSection
@@ -16,21 +23,30 @@ import com.mukmuk.todori.ui.theme.Dimens
 
 @Composable
 fun MyPageScreen(navController: NavController) {
-    // 테스트 유저
-    val currentUser = User(
-        uid = "123",
-        nickname = "asd",
-        intro = "asdasd",
-        level = 3,
-        rewardPoint = 1200
-    )
+    var currentUser by remember { mutableStateOf<User?>(null) }
+
+    LaunchedEffect(Unit) {
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        firebaseUser?.let { user ->
+            FirebaseFirestore.getInstance()
+                .collection("users")
+                .document("testuser")
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        currentUser = document.toObject(User::class.java)
+                    }
+                }
+        }
+    }
     Scaffold { padding ->
         Column(
             modifier = Modifier
                 .padding(Dimens.Medium)
         ) {
-            ProfileSection(user = currentUser)
-
+            currentUser?.let {
+                ProfileSection(user = it)
+            }
             Spacer(modifier = Modifier.height(Dimens.XXLarge))
 
             MyPageMenuSection(
