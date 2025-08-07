c73d8862aad9aba0f748a5defba19cbb033a20ab
lch <chlee1925@gmail.com>
2025-08-08 01:39:17 +0900

    [feature/STATS-007] TodoStatsService 파일 수정 - @RequiresApi 의 존재를 잠시 잊어서..

--- app/src/main/java/com/mukmuk/todori/data/service/TodoStatsService.kt
+++ app/src/main/java/com/mukmuk/todori/data/service/TodoStatsService.kt
@@ -1,18 +1,25 @@
 package com.mukmuk.todori.data.service
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mukmuk.todori.data.remote.todo.Todo
 import kotlinx.coroutines.tasks.await
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 class TodoStatsService @Inject constructor(
     private val firestore: FirebaseFirestore
 ) {
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun getTodosByMonth(uid: String, year: Int, month: Int): List<Todo> {
-        val startDateStr = "%04d-%02d-01".format(year, month)
-        val nextMonth = if (month == 12) 1 else month + 1
-        val nextYear = if (month == 12) year + 1 else year
-        val endDateStr = "%04d-%02d-01".format(nextYear, nextMonth)
+        val start = LocalDate.of(year, month, 1)
+        val end = start.plusMonths(1)
+
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val startDateStr = start.format(formatter)
+        val endDateStr = end.format(formatter)
 
         val snapshot = firestore
             .collection("users")
