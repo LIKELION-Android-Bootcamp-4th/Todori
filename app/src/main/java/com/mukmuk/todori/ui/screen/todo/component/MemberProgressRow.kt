1dfb3a3388fc73b25c1344bf99a418e90f5c50e8
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-08-11 11:23:47 +0900

    [feature/todo] Study Leader 아이콘 변경

--- app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/MemberProgressRow.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/MemberProgressRow.kt
@@ -10,18 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.EmojiEvents
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.mukmuk.todori.R
 import com.mukmuk.todori.data.remote.study.StudyMember
 import com.mukmuk.todori.ui.component.CustomLinearProgressBar
 import com.mukmuk.todori.ui.theme.AppTextStyle
@@ -56,12 +53,7 @@ fun MemberProgressRow(
         Column(modifier = Modifier.weight(1f)) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 if (member.role == "LEADER") {
-                    Icon(
-                        Icons.Default.EmojiEvents,
-                        tint = Color.Yellow,
-                        contentDescription = "Leader",
-                        modifier = Modifier.size(30.dp)
-                    )
+                    Image(painter = painterResource(R.drawable.leader),contentDescription = "Leader",modifier = Modifier.size(20.dp))
                     Spacer(modifier = Modifier.width(Dimens.Nano))
                 }
                 Text(member.nickname, style = AppTextStyle.Body.copy(fontWeight = FontWeight.Bold))
