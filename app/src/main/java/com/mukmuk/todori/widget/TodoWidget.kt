7fe5b8f23dcf801dfa11bfdf799fab3a74742319
mninm <kyhee2001@naver.com>
2025-08-14 13:40:17 +0900

    [feature/WIDGET-001] UI 정리

--- app/src/main/java/com/mukmuk/todori/widget/TodoWidget.kt
+++ app/src/main/java/com/mukmuk/todori/widget/TodoWidget.kt
@@ -1,21 +1,28 @@
 package com.mukmuk.todori.widget
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.layout.width
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
 import com.mukmuk.todori.R
 import com.mukmuk.todori.util.WidgetUtil
 
@@ -38,12 +45,24 @@ fun TodoWidgetContent(
             .fillMaxSize()
             .background(R.color.widgetBgColor),
     ) {
-        Column(modifier = GlanceModifier.padding(8.dp)) {
-            Row {
-                Text("TODO")
-                Spacer(GlanceModifier.width(10.dp))
-                Text("${todos.count { it.second }} / ${todos.size}")
+        Column(modifier = GlanceModifier.padding(16.dp)) {
+            Row(modifier = GlanceModifier.fillMaxWidth()) {
+                Text(
+                    "TODO", style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Spacer(GlanceModifier.defaultWeight())
+                Text(
+                    "${todos.count { it.second }} / ${todos.size}",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                )
             }
+            Spacer(GlanceModifier.height(10.dp))
             todos.take(3).forEach { (task, isDone) ->
                 Text(task)
             }
