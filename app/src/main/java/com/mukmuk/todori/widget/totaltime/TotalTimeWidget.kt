476152dffa4ce8f14cd41a9e2fdfed19c9702dba
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-08-17 18:07:12 +0900

    [Feat/WGT-001] WidgetEntryPoint 추가
    위젯에선 Hilt 주입 바로 불가능 -> EntryPoint 사용해 싱글톤 Repository 가져옴

--- app/src/main/java/com/mukmuk/todori/widget/totaltime/TotalTimeWidget.kt
+++ app/src/main/java/com/mukmuk/todori/widget/totaltime/TotalTimeWidget.kt
@@ -1,6 +1,8 @@
-package com.mukmuk.todori.widget
+package com.mukmuk.todori.widget.totaltime
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
@@ -12,25 +14,40 @@ import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import com.mukmuk.todori.data.local.datastore.RecordSettingRepository
 import com.mukmuk.todori.ui.theme.WidgetTextStyle
+import com.mukmuk.todori.widget.WidgetEntryPoint
+import dagger.hilt.android.EntryPointAccessors
+import java.time.LocalDate
+import javax.inject.Inject
 
 class TotalTimeWidget : GlanceAppWidget() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        val hiltEntryPoint = EntryPointAccessors.fromApplication(
+            context.applicationContext,
+            WidgetEntryPoint::class.java
+        )
+        val repository = hiltEntryPoint.recordSettingRepository()
+
+        val today = LocalDate.now().toString()
+        val totalTime = repository.getTotalTime()
+
         provideContent {
-            TotalTimeWidgetContent()
+            TotalTimeWidgetContent(today, totalTime)
         }
     }
 
     @Composable
-    private fun TotalTimeWidgetContent() {
+    private fun TotalTimeWidgetContent(today: String, totalTime: String) {
         Row (
             modifier = GlanceModifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "0000년 00월 00일", modifier = GlanceModifier.padding(12.dp), style = WidgetTextStyle.TitleSmallLight)
-            Text(text = "00:00:00", modifier = GlanceModifier.padding(12.dp), style = WidgetTextStyle.TitleLarge)
+            Text(text = today, modifier = GlanceModifier.padding(12.dp), style = WidgetTextStyle.TitleSmallLight)
+            Text(text = totalTime, modifier = GlanceModifier.padding(12.dp), style = WidgetTextStyle.TitleLarge)
         }
     }
 }
--- app/src/main/java/com/mukmuk/todori/widget/totaltime/TotalTimeWidgetReceiver.kt
+++ app/src/main/java/com/mukmuk/todori/widget/totaltime/TotalTimeWidgetReceiver.kt
@@ -2,7 +2,6 @@ package com.mukmuk.todori.widget.totaltime
 
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import com.mukmuk.todori.widget.TotalTimeWidget
 
 class TotalTimeWidgetReceiver: GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = TotalTimeWidget()
