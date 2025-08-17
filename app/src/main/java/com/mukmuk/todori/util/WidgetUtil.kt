dfb9bc2261f7c368914b7aa1d8e0aa3475d8d28d
mninm <kyhee2001@naver.com>
2025-08-17 14:47:32 +0900

    [feature/WIDGET-001] uid 받아오기

--- app/src/main/java/com/mukmuk/todori/util/WidgetUtil.kt
+++ app/src/main/java/com/mukmuk/todori/util/WidgetUtil.kt
@@ -2,13 +2,20 @@ package com.mukmuk.todori.util
 
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
+import java.time.LocalDate
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 object WidgetUtil {
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend fun loadWidgetTodos(uid: String): List<Pair<String, Boolean>> {
+        val today = LocalDate.now()
+        val todayStr = today.toString()
         val snapshot = FirebaseFirestore.getInstance()
             .collection("users")
             .document(uid)
             .collection("todos")
+            .whereEqualTo("date", todayStr)
             .get()
             .await()
 
--- app/src/main/java/com/mukmuk/todori/widget/TodoWidgetWorker.kt
+++ app/src/main/java/com/mukmuk/todori/widget/TodoWidgetWorker.kt
@@ -7,6 +7,7 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 import com.google.firebase.functions.dagger.assisted.Assisted
 import com.google.firebase.functions.dagger.assisted.AssistedInject
@@ -20,6 +21,9 @@ class TodoWidgetWorker @AssistedInject constructor(
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         return try {
+            val currentUser = Firebase.auth.currentUser
+            val uid = currentUser?.uid ?: return Result.failure()
+
             val db = Firebase.firestore
             val snapshot = db.collection("todos")
                 .whereEqualTo("done", false)
@@ -28,11 +32,11 @@ class TodoWidgetWorker @AssistedInject constructor(
 
             val todos = snapshot.documents.map { doc ->
                 val title = doc.getString("title") ?: ""
-                val done = doc.getBoolean("done") ?: false
-                title to done
+                val completed = doc.getBoolean("completed") ?: false
+                title to completed
             }
 
-            WidgetUtil.loadWidgetTodos("Ek8NgW6xi1fGTb0kXqHowxUweqG3")
+            WidgetUtil.loadWidgetTodos(uid)
             Result.success()
         } catch (e: Exception) {
             e.printStackTrace()
