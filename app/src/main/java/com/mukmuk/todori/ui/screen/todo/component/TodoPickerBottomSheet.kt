4e64f69551971f4b98d87835499090e83115a4fd
groundinsider <groundinsider24@gmail.com>
2025-08-19 10:38:31 +0900

    [feat/TODO-013] 보낼 category 선택 알림

--- app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/TodoPickerBottomSheet.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/TodoPickerBottomSheet.kt
@@ -1,8 +1,21 @@
 package com.mukmuk.todori.ui.screen.todo.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mukmuk.todori.data.remote.todo.TodoCategory
+import com.mukmuk.todori.ui.theme.AppTextStyle
+import com.mukmuk.todori.ui.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoPickerBottomSheet (
 
@@ -12,6 +25,40 @@ fun TodoPickerBottomSheet (
 
 ) {
 
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
 
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        sheetState = sheetState,
+        containerColor = White
+    ) {
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "카테고리 선택",
+                style = AppTextStyle.TitleMedium,
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            LazyColumn(
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                items(todoCategoryList.size) {
+                    TodoCategoryItem(
+                        todoCategory = todoCategoryList[it],
+                        onClick = {
+                            onSelected(todoCategoryList[it])
+                            onDismiss()
+                        }
+                    )
+            }
+
+        }
+
+    }
 
 }
