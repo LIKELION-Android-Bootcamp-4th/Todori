1714807e434cfefdb1022ae37d1b514fd1e52a44
groundinsider <groundinsider24@gmail.com>
2025-08-21 10:44:29 +0900

    [Feat/TODO-013] TodoCategory 선택 알림 추가

--- app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/TodoCategoryPickerBottomSheet.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/todo/component/TodoCategoryPickerBottomSheet.kt
@@ -1,9 +1,11 @@
 package com.mukmuk.todori.ui.screen.todo.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.mukmuk.todori.data.remote.todo.Todo
 import com.mukmuk.todori.data.remote.todo.TodoCategory
 import com.mukmuk.todori.ui.theme.AppTextStyle
 import com.mukmuk.todori.ui.theme.White
@@ -20,17 +23,22 @@ import com.mukmuk.todori.ui.theme.White
 fun TodoCategoryPickerBottomSheet (
 
     todoCategoryList: List<TodoCategory>,
+    todosMap: Map<String, List<Todo>>,
+    show: Boolean,
     onSelected: (TodoCategory) -> Unit,
-    onDismiss: () -> Unit
+    onDismissRequest: () -> Unit,
 
-) {
+    ) {
 
     val sheetState = rememberModalBottomSheetState(
         skipPartiallyExpanded = true
     )
 
+    if(show){
     ModalBottomSheet(
-        onDismissRequest = onDismiss,
+        modifier = Modifier.fillMaxHeight()
+            .padding(top = 120.dp),
+        onDismissRequest = onDismissRequest,
         sheetState = sheetState,
         containerColor = White
     ) {
@@ -47,18 +55,26 @@ fun TodoCategoryPickerBottomSheet (
             LazyColumn(
                 modifier = Modifier.padding(top = 16.dp)
             ) {
-                items(todoCategoryList.size) {
-                    TodoCategoryItem(
-                        todoCategory = todoCategoryList[it],
-                        onClick = {
-                            onSelected(todoCategoryList[it])
-                            onDismiss()
+                items(todoCategoryList) { category ->
+                    TodoCategoryListData(
+
+                        categoryTitle = category.name,
+                        description = category.description ?: "",
+                        todos = todosMap[category.categoryId] ?: emptyList(),
+                        onSelected = {
+                            onSelected(category)
                         }
+
+
                     )
+                }
+
             }
 
         }
+    }
 
     }
+
 
 }
