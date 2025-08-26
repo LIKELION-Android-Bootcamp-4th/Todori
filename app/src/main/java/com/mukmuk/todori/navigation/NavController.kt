376972f55ddf39261f23ef795179ff03610744a3
lch <chlee1925@gmail.com>
2025-08-27 08:41:08 +0900

    [fix/btm-nav] 백스택 fix - A가 홈화면일때 A >B >C >B >D에서 뒤로가기 누를시 D > A > 앱 종료

--- app/src/main/java/com/mukmuk/todori/navigation/NavController.kt
+++ app/src/main/java/com/mukmuk/todori/navigation/NavController.kt
@@ -3,7 +3,6 @@ package com.mukmuk.todori.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 
-/** 항상 home(A)만 바닥에 남기고 target 탭으로 이동 */
 fun NavController.navigateKeepingHomeBase(
     targetRoute: String,
     todoRoute: String = BottomNavItem.Todo.route
@@ -13,9 +12,6 @@ fun NavController.navigateKeepingHomeBase(
             inclusive = false
             saveState = false
         }
-        // 만약 위 popUpTo(String) 이 버전 이슈로 안 되면 아래로 대체:
-        // val homeId = this@navigateKeepingHomeBase.graph.findNode(homeRoute)?.id
-        // if (homeId != null) { popUpTo(homeId) { inclusive = false; saveState = false } }
 
         launchSingleTop = true
         restoreState = false
