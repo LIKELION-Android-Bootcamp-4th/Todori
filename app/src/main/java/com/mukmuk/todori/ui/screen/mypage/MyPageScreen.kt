42f236be85a3d0cf016d77bb49447bf6c69cd3ef
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-08-27 17:48:43 +0900

    [fix/profile] profile 불러오는 동안 isLoading 값을 이용한 indicator 설정

--- app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/mypage/MyPageScreen.kt
@@ -1,11 +1,14 @@
 package com.mukmuk.todori.ui.screen.mypage
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -16,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -75,31 +79,42 @@ fun MyPageScreen(
     }
 
     Scaffold { padding ->
-        Column(
-            modifier = Modifier
-                .padding(Dimens.Medium)
-        ) {
-            state.user?.let { ProfileSection(user = it) }
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            Column(
+                modifier = Modifier
+                    .padding(Dimens.Medium)
+            ) {
+                state.user?.let { ProfileSection(user = it) }
 
-            Spacer(modifier = Modifier.height(Dimens.XXLarge))
+                Spacer(modifier = Modifier.height(Dimens.XXLarge))
 
-            MyPageMenuSection(
-                onLevelClick = {navController.navigate("myLevel")},
-                onGoalClick = {navController.navigate("completedGoals")},
-                onProfileEditClick = {navController.navigate("profileManage")},
-            )
+                MyPageMenuSection(
+                    onLevelClick = { navController.navigate("myLevel") },
+                    onGoalClick = { navController.navigate("completedGoals") },
+                    onProfileEditClick = { navController.navigate("profileManage") },
+                )
 
-            Spacer(modifier = Modifier.height(Dimens.XXLarge))
+                Spacer(modifier = Modifier.height(Dimens.XXLarge))
 
-            MyPageAccountSection(
-                onLogoutClick = {
-                    showLogoutDialog = true
-                },
-                onDeleteAccountClick = {
-                    showDeleteDialog = true
-                },
-                onStudyTargetsClick = {navController.navigate("studyTargets")}
-            )
+                MyPageAccountSection(
+                    onLogoutClick = {
+                        showLogoutDialog = true
+                    },
+                    onDeleteAccountClick = {
+                        showDeleteDialog = true
+                    },
+                    onStudyTargetsClick = { navController.navigate("studyTargets") }
+                )
+            }
         }
     }
     if (showDeleteDialog) {
