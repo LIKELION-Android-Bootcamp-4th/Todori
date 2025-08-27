04ef88a116a2ad5e0a8f780b6d17b7802c7e430f
groundinsider <groundinsider24@gmail.com>
2025-08-27 11:33:04 +0900

    [Feat/TODO-013] 상세에서 TODO 데이터

--- app/src/main/java/com/mukmuk/todori/ui/screen/todo/list/todo/TodoListViewModel.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/todo/list/todo/TodoListViewModel.kt
@@ -2,6 +2,7 @@ package com.mukmuk.todori.ui.screen.todo.list.todo
 
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -30,6 +31,11 @@ class TodoListViewModel @Inject constructor(
     private val _state = MutableStateFlow(TodoListState())
     val state: StateFlow<TodoListState> = _state.asStateFlow()
 
+    fun setSelectedOption(option: String) {
+        if(_state.value.selectedOption == option) return
+        _state.update { it.copy(selectedOption = option) }
+        clearLists()
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun loadTodoList(uid: String, selectedDate: LocalDate) {
@@ -62,33 +68,53 @@ class TodoListViewModel @Inject constructor(
             _state.update { it.copy(isLoading = true) }
             try {
 
-                val categories = categoryRepository.getSendCategories(uid)
+                val sendCategories = categoryRepository.getSendCategories(uid)
 
-                val updatedCategories = categories.map { category ->
-                    categoryRepository.getCategoryById(category.uid, category.categoryId)
+                val userMap: MutableMap<String, String> = mutableMapOf()
+
+                for(category in sendCategories) {
+                    val user = categoryRepository.getUserById(category.uid)
+                    userMap[category.categoryId] = user?.nickname ?: ""
                 }
 
                 val todoMap: MutableMap<String, List<Todo>> = mutableMapOf()
 
-                for (category in updatedCategories) {
-                    val todos = todoRepository.getTodosByDate(category?.uid ?: "", selectedDate)
-                    todoMap.putAll(todos.groupBy { it.categoryId })
+                for (category in sendCategories) {
+                    val todos = todoRepository.getTodosByDate(category.uid, selectedDate)
+                    todoMap[category.categoryId] = todos.groupBy { it.categoryId }[category.categoryId] ?: emptyList()
                 }
+
+
+
 
                 _state.update {
                     it.copy(
                         isLoading = false,
-                        sendCategories = updatedCategories as List<TodoCategory>,
+                        userMap = userMap,
+                        sendCategories = sendCategories,
                         sendTodosByCategory = todoMap,
                         selectedDate = selectedDate
                     )
                 }
             } catch (e: Exception) {
+                Log.e("TodoVM", "loadSendTodoList ERROR", e)
                 _state.update { it.copy(isLoading = false, error = e.message) }
             }
         }
 
 
 
+    }
+
+
+    fun clearLists() {
+        _state.update {
+            it.copy(
+                categories = emptyList(),
+                todosByCategory = emptyMap(),
+                sendCategories = emptyList(),
+                sendTodosByCategory = emptyMap()
+            )
+        }
     }
 }
