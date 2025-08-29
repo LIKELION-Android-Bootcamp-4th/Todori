e5e3339d684eaef3c885528e23fc12d500fa46e3
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-08-29 14:35:44 +0900

    [refactor/stat] 카테고리별 랭킹

--- app/src/main/java/com/mukmuk/todori/ui/screen/stats/component/report/MonthHeroCard.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/stats/component/report/MonthHeroCard.kt
@@ -21,12 +21,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.mukmuk.todori.ui.theme.AppTextStyle
 import com.mukmuk.todori.ui.theme.Black
 import com.mukmuk.todori.ui.theme.Dimens
 import com.mukmuk.todori.ui.theme.GoalPrimary
+import com.mukmuk.todori.ui.theme.LightYellow
 import com.mukmuk.todori.ui.theme.StudyStreakOrange
 import com.mukmuk.todori.ui.theme.White
 
@@ -35,13 +36,14 @@ fun MonthHeroCard(
     topCategories: List<CategoryProgress>
 ) {
     val topCategory = topCategories.firstOrNull()
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = Dimens.Medium),
         shape = Dimens.CardDefaultRadius,
         colors = CardDefaults.cardColors(
-            containerColor = Color(0xFFFFF8E1)
+            containerColor = LightYellow
         ),
         border = BorderStroke(1.dp, GoalPrimary)
     ) {
@@ -71,15 +73,23 @@ fun MonthHeroCard(
                         style = AppTextStyle.TitleSmall,
                         color = Black
                     )
-                    Text(
-                        text = if (topCategory != null) {
-                            "'${topCategory.name}' - ${topCategory.completionRate}%"
-                        } else {
-                            ""
-                        },
-                        style = AppTextStyle.BodySmallNormal,
-                        color = StudyStreakOrange,
-                    )
+                    if (topCategory != null) {
+                        Text(
+                            text = "'${topCategory.name}' - ${topCategory.completionRate}%",
+                            style = AppTextStyle.BodySmallNormal,
+                            color = StudyStreakOrange,
+                            modifier = Modifier.fillMaxWidth(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    } else {
+                        Text(
+                            text = "데이터가 부족해 통계를 내기 어려워요.",
+                            style = AppTextStyle.BodySmallBold,
+                            color = Black.copy(alpha = 0.6f),
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 }
             }
 
