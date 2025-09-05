e4929106705e7a716a3dbae6702c5551b0965cd1
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-09-05 18:22:24 +0900

    [chore/cleanup] ui/screen/login cleanup

--- app/src/main/java/com/mukmuk/todori/ui/screen/login/LoginEvent.kt
+++ app/src/main/java/com/mukmuk/todori/ui/screen/login/LoginEvent.kt
@@ -3,7 +3,4 @@ package com.mukmuk.todori.ui.screen.login
 sealed class LoginEvent {
     data class GoogleLogin(val idToken: String) : LoginEvent()
     data class LoginFailure(val errorMessage: String) : LoginEvent()
-
-    object KakaoLogin : LoginEvent()
-    object NaverLogin : LoginEvent()
 }
